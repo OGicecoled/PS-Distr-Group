59ab42ad47eccca9b6809fffaf9a579a4e145e23
OGicecoled <60413787+OGicecoled@users.noreply.github.com>
2020-04-15 15:06:47 -0400

    Add files via upload

--- All2GroupCheckPS.ps1
+++ All2GroupCheckPS.ps1
@@ -4,11 +4,10 @@ $all2CSV = Import-csv -path C:\users\ceverett\desktop\All2.csv
 
 # Import All2 group, ignores the users in the group without an external email address (All, BHorn)
 $all2group = Get-DistributionGroupMember -identity all2 | Where-Object {$_.ExternalEmailAddress -ne $null}
-$all2group = $_.ExternalEmailAddress -replace ".*:"
 
 $Compare = Compare-Object $all2CSV $all2group -property ExternalEmailAddress -IncludeEqual
 
-# Identifies which users should be added or kept. Automatically removes the users who should be removed.
+# Identifies which users should be added, removed, kept. Automatically removes the users who should be removed.
 $Compare | foreach {
     if ($_.sideindicator -eq '<=')
         {$_.sideindicator = 'Add'}
@@ -18,6 +17,7 @@ $Compare | foreach {
 
     if ($_.sideindicator -eq '=>')
         {
+        $_.ExternalEmailAddress = $_.ExternalEmailAddress -replace ".*:"
         Remove-DistributionGroupMember -identity "All2" -member $_.ExternalEmailAddress
         }
 }
